8034a1ca90e42725208f5306e8839883863539f1
Mitchell Wand <wand@ccs.neu.edu>
2021-01-03 21:51:22 -0500

    mostly working, except for clearing the input box

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,92 @@
-import * as React from "react"
+import React from "react";
 import {
   ChakraProvider,
-  Box,
+  Button,
+  Heading,
+  Stack,
+  Table,
+  Td,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Tr,
+  Th,
+  Input,
+  FormLabel
+} from "@chakra-ui/react";
+import customTheme from "./customTheme";
+import { useForm } from "react-hook-form";
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+type Task = { id: number, description: string }
+
+export default function App() {
+
+  const { handleSubmit, errors, register, formState } = useForm();
+  const [count, setCount] = React.useState<number>(0)
+  const [tasks, setTasks] = React.useState<Task[]>([])
+
+  function onSubmit(values: { name: string }) {
+    setCount(count + 1)  // doesn't take effect immediately!
+    setTasks(tasks.concat({ id: count, description: values.name }))
+  }
+
+  function resetTasks() { setTasks([]) }   // doesn't reset the count
+
+  // renders the task as a table row, with its own delete button
+  function renderTask(t: Task) {
+    function deleteThisTask() {
+      setTasks(tasks.filter(t0 => (t.id !== t0.id)))
+    }
+    return (
+      <Tr>
+        <Td width="200px">{t.id}</Td>
+        <Td>{t.description}</Td>
+        <Td> <Button width="60px" size="md" onClick={deleteThisTask}>Delete</Button> </Td>
+      </Tr>
+    )
+  }
+
+  // make tasks into a table
+  function renderTasks() {
+    return (
+      <div>
+        <Text size="md">My Tasks:</Text>
+        <Table size="sm" variant="striped" colorScheme="teal">
+          <Th><Td>Id</Td><Td>Description</Td></Th>
+          {tasks.map(renderTask)}
+        </Table>
+      </div>
+    )
+
+  }
+
+
+  function renderForm() {
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormLabel>Enter new task here</FormLabel>
+          <Input
+            name="name"
+            placeholder="new task"
+            ref={register}
+            width="80%"
+          />
+          <Button mt={0} colorScheme="teal" isLoading={formState.isSubmitting} type="submit">
+            Submit
+          </Button>
+        </form>
+    )
+  }
+
+  // put the ChakraProvider here so you can specify the theme
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Stack spacing={4} bg="white" p={8} borderRadius="lg">
+        <Heading as="h1" size="lg" color="primary.900">
+          My ToDo App
+        </Heading>
+        {renderForm()}
+        {renderTasks()}
+        <Button width="150px" onClick={resetTasks}>delete all tasks </Button>
+      </Stack>
+    </ChakraProvider>
+  );
+}
